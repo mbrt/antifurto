b21af8a8b72b215e7ead05505da89d98611f3bb5
Michele Bertasi <michele.bertasi@gmail.com>
2014-05-14 23:01:21 +0200

    Fixed scripts

--- scripts/setup-sysroot.sh
+++ scripts/setup-sysroot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sshfs -o allow_other -o IdentityFile=~/.ssh/id_rsa ${TARGET_HOST} ${DEVICE_ROOT_FS}
+sshfs -o allow_other -o IdentityFile=~/.ssh/id_rsa ${TARGET_HOST}:/ ${DEVICE_ROOT_FS}
