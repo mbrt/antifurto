fd2128c1e9a19897e110557cc6eff2cae0b3c5ca
Michele Bertasi <michele.bertasi@gmail.com>
2014-06-18 18:12:09 +0200

    Removed boost log

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,31 +1,9 @@
 #include "Log.hpp"
-#include "StaticConfig.hpp"
-
-#include <boost/filesystem.hpp>
-#include <boost/log/utility/setup/file.hpp>
+#include "Config.hpp"
 
 namespace antifurto {
 
-using namespace boost;
-
-void initLogger(const Configuration& config)
-{
-    filesystem::path logFile{ config::logDir() };
-    logFile /= "antifurto_%Nlog";
-    log::add_file_log(
-        log::keywords::file_name = logFile.string(),
-        log::keywords::rotation_size = 100 * 1024,      // rotate every 100K
-        log::keywords::format = "[%TimeStamp%]: %Message%"
-    );
-    // determine log severity
-    log::trivial::severity_level level;
-    if (config.log.level == Configuration::Log::Level::DEBUG)
-        level = log::trivial::debug;
-    else
-        level = log::trivial::info;
-    log::core::get()->set_filter(
-        log::trivial::severity >= level
-    );
-}
+void initLogger(const Configuration& )
+{ }
 
 } // namespace antifurto
