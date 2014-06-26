e9de1e725c999c23ddd7fb31ab4903ab0d7afb10
Michele Bertasi <michele.bertasi@gmail.com>
2014-06-26 23:10:44 +0200

    Fixed build for Pi

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cp antifurto ${DEPLOY_DIR}
-cp test/antifurto_pi_test ${DEPLOY_DIR}
+cp ${CMAKE_CURRENT_BINARY_DIR}/antifurto ${DEPLOY_DIR}
+cp ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/yowsup-cli ${DEPLOY_DIR}
+cp -r ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/Yowsup ${DEPLOY_DIR}/Yowsup
+cp -r ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/Examples ${DEPLOY_DIR}/Examples
+cp ${CMAKE_CURRENT_SOURCE_DIR}/lib/dropbox_uploader/dropbox_uploader.sh ${DEPLOY_DIR}
+#cp test/antifurto_pi_test ${DEPLOY_DIR}
