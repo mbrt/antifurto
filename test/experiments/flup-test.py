a439cbf0f83aa684ca2ae5ab9051b5d2d72a1448
Michele Bertasi <michele.bertasi@gmail.com>
2014-08-22 18:59:17 +0200

    [test] Added flup experiment (failed for now)

---
+++ test/experiments/flup-test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+def myapp(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['Hello World!\n']
+
+if __name__ == '__main__':
+    from flup.server.fcgi import WSGIServer
+    WSGIServer(myapp, bindAddress='/tmp/myapp-fastcgi.socket').run()
