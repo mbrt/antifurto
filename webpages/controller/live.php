71b6e1a3cf8a988741af01bdf8933dcca46181a5
Michele Bertasi <michele.bertasi@gmail.com>
2014-09-09 07:28:41 +0200

    [webpages] Template for archive

--- webpages/controller/live.php
+++ webpages/controller/live.php
@@ -1,5 +1,5 @@
 <?php
     exec('sudo killall -37 antifurto 2>&1', $out, $retvar);
     // write json result
-    echo '{ "result": ' . $retvar . ', "log": "' . $out[0] . '" }';
+    echo json_encode(array('result' => $retvar, 'log' => $out[0]));
 ?>
