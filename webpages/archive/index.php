c17f7ff121e64e982a83a3abeb9d07e5f7f25705
Michele Bertasi <michele.bertasi@gmail.com>
2014-09-13 20:36:57 +0200

    [webpages] wip on porting archive to php

--- webpages/archive/index.php
+++ webpages/archive/index.php
@@ -1,3 +1,89 @@
+<?php
+    // settings
+    $pics_path = 'pics';
+    $pics_per_page = 16;
+
+    // helper functions
+    function get_or_default(&$var, $default = null) {
+        return isset($var) ? $var : $default;
+    }
+
+    // input: yyyymmdd
+    // output: (yyyy, mm, dd)
+    function split_compact_date($from) {
+        $yyyy = substr($from, 0, 4);
+        $mm = substr($from, 4, 2);
+        $dd = substr($from, 6, 2);
+        return array($yyyy, $mm, $dd);
+    }
+
+    // input: yyyy?mm?dd
+    // output: yyyymmdd
+    function compact_date($date) {
+        $yyyy = substr($date, 0, 4);
+        $mm = substr($date, 5, 2);
+        $dd = substr($date, 8, 2);
+        return "$yyyy$mm$dd";
+    }
+
+    // input: yyyy?mm?dd
+    // output: dd/mm/yyyy
+    function format_day_dir($dir) {
+        $a = split_compact_date(compact_date($dir));
+        return $a[2] .'/'. $a[1] .'/'. $a[0];
+    }
+
+    // count files in directory
+    function count_files($dir) {
+        $count = 0;
+        if ($handle = opendir($dir))
+            while (false !== ($entry = readdir($handle)))
+                if ($entry != '.' && $entry != '..')
+                    $count++;
+        return $count;
+    }
+
+    // globals
+	$today_dd = date("d", time());
+	$today_mm = date("m", time());
+	$today_yyyy = date("Y", time());
+	$today = "$today_yyyy$today_mm$today_dd";
+	$today_dir = "$today_yyyy-$today_mm-$today_dd";
+
+    // parameters
+    $page = get_or_default($_GET['page'], 1);
+    $sel_date = get_or_default($_GET['day'], $today);
+    if (strlen($sel_date) < 8)
+        $sel_date = $today;
+
+    $sel_year = substr($sel_date, 0, 4);
+    $sel_month = substr($sel_date, 4, 2);
+    $sel_day = substr($sel_date, 6, 2);
+
+    // collect days from newer to older
+    $days = array();
+    if ($pics_handle = opendir($pics_path)) {
+        while (false !== ($entry = readdir($pics_handle)))
+            if ($entry != '.' && $entry != '..')
+                array_push($days, $entry);
+        rsort($days);
+    }
+
+    // collect pictures of selected day from older to newer
+    $all_pics = array();
+    if ($pics_handle = opendir($sel_date)) {
+        while (false !== ($entry = readdir($pics_handle)))
+            if ($entry != '.' && $entry != '..')
+                array_push($all_pics, $entry);
+        sort($all_pics);
+    }
+
+    $num_pages = ceil(floatval(sizeof($all_pics)) / $pics_per_page);
+    if ($page > $num_pages)
+        $page = 1;
+    $first_pic_index = ($page - 1) * $pics_per_page;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -61,14 +147,36 @@
         <div class="col-sm-4 text-center">
           <div class="btn-group days">
             <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-              Today <span class="caret"></span>
+              <?php
+                if ($sel_date == $today)
+                    echo 'Today';
+                else
+                    echo "$sel_day/$sel_month/$sel_year";
+                echo ' <span class="caret"></span>';
+              ?>
             </button>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="#"><span class="badge pull-right">56</span>Today</a></li>
-              <li class="divider"></li>
-              <li><a href="#"><span class="badge pull-right">30</span>09/09/2014</a></li>
-              <li><a href="#"><span class="badge pull-right">150</span>15/08/2014</a></li>
-              <li><a href="#"><span class="badge pull-right">332</span>14/08/2014</a></li>
+              <?php
+                function gen_day_li_link($day_dir, $day_display, $link) {
+                    global $pics_path;
+                    $num_pics = count_files("$pics_path/$day_dir");
+                    echo '<li><a href="'. $link .'">';
+                    if ($num_pics > 0)
+                        echo '<span class="badge pull-right">'. $num_pics .'</span>';
+                    echo "$day_display</a></li>";
+                }
+                function gen_day_li($day_dir, $day_display) {
+                    gen_day_li_link($day_dir, $day_display, '?day='. compact_date($day_dir));
+                }
+
+                gen_day_li_link($today_dir, 'Today', '?');
+                if (sizeof($days) > 1 || (sizeof($days) == 1 && $days[0] != $today_dir))
+                    echo '<li class="divider"></li>';
+                if (sizeof($days) > 0 && $days[0] != $today_dir)
+                    gen_day_li($days[0], format_day_dir($days[0]));
+                for ($i = 1; $i < sizeof($days); $i++)
+                    gen_day_li($days[$i], format_day_dir($days[$i]));
+              ?>
             </ul>
           </div>
         </div>
@@ -76,13 +184,13 @@
         <!-- pages -->
         <div class="col-sm-8 text-center">
           <ul class="pagination archive-pages">
-            <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span> Older</a></li>
+            <li class="disabled"><a href="#"><span class="glyphicon glyphicon-chevron-left"></span><span class="hidden-xs"> Older</span></a></li>
             <li class="active"><a href="#">1</a></li>
             <li><a href="#">2</a></li>
             <li><a href="#">3</a></li>
-            <li class="hidden-xs"><a href="#">4</a></li>
-            <li class="hidden-xs"><a href="#">5</a></li>
-            <li><a href="#">Newer <span class="glyphicon glyphicon-chevron-right"></span></a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#"><span class="hidden-xs">Newer </span><span class="glyphicon glyphicon-chevron-right"></span></a></li>
           </ul>
         </div>
       </div>
