56852e13c3ea46083668c27110ee463f89fbc739
Michele Bertasi <michele.bertasi@gmail.com>
2014-09-17 19:11:08 +0200

    wip on Log class

--- src/Log.cpp
+++ src/Log.cpp
@@ -29,3 +29,188 @@ void initLogger(const Configuration& config)
 }
 
 } // namespace antifurto
+
+// END OF OLD STUFF
+
+#include <iostream>
+#include <fstream>
+#include <mutex>
+
+namespace antifurto {
+namespace log {
+namespace { // anon
+
+// see http://stackoverflow.com/questions/19200207/redirect-debug-output-to-null-stream-instead-of-stdcerr
+template <class cT, class traits = std::char_traits<cT> >
+class basic_nullbuf: public std::basic_streambuf<cT, traits> {
+    typename traits::int_type overflow(typename traits::int_type c)
+    {
+        return traits::not_eof(c); // indicate success
+    }
+};
+
+template <class cT, class traits = std::char_traits<cT> >
+class basic_onullstream: public std::basic_ostream<cT, traits> {
+public:
+    basic_onullstream():
+    std::basic_ios<cT, traits>(&m_sbuf),
+    std::basic_ostream<cT, traits>(&m_sbuf)
+    {
+        this->init(&m_sbuf);
+    }
+
+private:
+    basic_nullbuf<cT, traits> m_sbuf;
+};
+
+typedef basic_onullstream<char> onullstream;
+typedef basic_onullstream<wchar_t> wonullstream;
+
+constexpr int numLevels() {
+    return 3;
+}
+
+/// This is a no op sink
+class NullSink : public LoggerSink
+{
+    std::ostream& getStream() override {
+        return nullStream;
+    }
+    void lock() override { }
+    void unlock() override { }
+    bool isValid() const {
+        return false;
+    }
+    const char* getName() const override {
+        return "";
+    }
+    void setName(const char*) override { }
+};
+
+/// This is a valid sink, but it does not know its stream
+class ValidSink : public LoggerSink
+{
+    ValidSink(const char* name = "")
+        : name_(name) { }
+    
+    void lock() override {
+        mutex.lock();
+    }
+    void unlock() override {
+        mutex.unlock();
+    }
+    bool isValid() const {
+        return true;
+    }
+    const char* getName() const override {
+        return name_;
+    }
+    void setName(const char* name) override {
+        name_ = name;
+    }
+    
+    const char* name_;
+};
+
+/// This sink writes to file
+class FileSink : public ValidSink
+{
+    using ValidSink::ValidSink;
+    
+    std::ostream& getStream() override {
+        return fileStream;
+    }
+};
+
+/// This sink writes to std::cerr
+class CerrSink : public ValidSink
+{
+    using ValidSink::ValidSink;
+    
+    std::ostream& getStream() override {
+        return std::cerr;
+    }
+}
+
+
+// unique sinks
+NullSink nullSink;
+std::array<CerrSink, numLevels()> defaultSinkVector = {
+    CerrSink{"ERROR"},
+    CerrSink{"INFO"},
+    CerrSink{"DEBUG"}
+};
+
+// global state
+std::mutex mutex;
+std::ofstream fileStream;
+std::onullstream nullStream;
+std::array<LoggerSink*, numLevels()> sinkVector = {
+    &defaultSinkVector[0],
+    &defaultSinkVector[1],
+    &defaultSinkVector[2]
+};
+
+// settings
+std::string logFilePath;
+
+Logger getLogger(int level)
+{
+    return Logger{sinkVector[level]};
+}
+
+} // anon namespace
+
+
+Logger::Logger(LoggerSink* sink)
+    : sink_(sink)
+{
+    if (sink_->isValid()) {
+        sink_->lock();
+        std::ostream& out = sink_->getStream();
+        // dump date time
+        // ...
+        out << sink_->getName() << ": ";
+    }
+}
+
+Logger::~Logger()
+{
+    if (sink_ && sink_->isValid()) {
+        std::ostream& out = sink_->getStream();
+        out << std::endl;
+        out.flush();
+        sink_->unlock();
+    }
+}
+
+
+void init(const Configuration& config)
+{
+    // ...
+    nullStream.setstate(std::ios_base::failbit);
+}
+
+void reload()
+{
+    std::unique_lock<std::mutex> lock(mutex);
+    fileStream.close();
+    fileStream.open(logFilePath.c_str());
+}
+
+Logger error()
+{
+    return getLogger(0);
+}
+
+Logger info()
+{
+    return getLogger(1);
+}
+
+Logger debug()
+{
+    return getLogger(2);
+}
+
+}} // namespace antifurto::log
--- src/Log.hpp
+++ src/Log.hpp
@@ -18,3 +18,61 @@ class Configuration;
 void initLogger(const Configuration& config);
 
 } // namespace antifurto
+
+
+// END OF OLD STUFF
+//
+#include <iosfwd>
+#include <utility>
+
+namespace antifurto {
+
+// fwd reference
+class Configuration;
+
+namespace log {
+
+class LoggerSink
+{
+public:
+    virtual std::ostream& getStream();
+    virtual void lock();
+    virtual void unlock();
+    virtual bool isValid() const;
+    virtual const char* getName() const;
+    virtual void setName(const char*);
+    virtual ~LoggerSink() {}
+};
+
+class Logger
+{
+public:
+    Logger(LoggerSink* sink);
+    ~Logger();
+    
+    Logger(const Logger& ) = delete;
+    Logger& operator= (const Logger& ) = delete;
+    
+    Logger(Logger&& other) : sink_(other.sink_) { other.sink_ = nullptr; }
+    Logger& operator= (Logger&& other) {
+        std::swap(other.sink_, sink_);
+        return *this;
+    }
+    
+    template <typename T>
+    Logger& operator<< (T&& v) {
+        sink_->getStream() << std::forward(v);
+        return *this;
+    }
+
+private:
+    LoggerSink* sink_;
+};
+
+void init(const Configuration& config);
+void reload();
+Logger error();
+Logger info();
+Logger debug();
+
+}} // namespace antifurto::log
