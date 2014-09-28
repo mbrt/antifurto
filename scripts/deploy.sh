839950e5e15d357a7728e6dea29b8769affb2775
Michele Bertasi <michele.bertasi@gmail.com>
2014-09-28 20:30:50 +0200

    [frontend] Fixed timeout and lighttpd config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cp ${CMAKE_CURRENT_BINARY_DIR}/antifurto ${DEPLOY_DIR}
-cp ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/yowsup-cli ${DEPLOY_DIR}
-cp -r ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/Yowsup ${DEPLOY_DIR}/Yowsup
-cp -r ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/Examples ${DEPLOY_DIR}/Examples
-cp ${CMAKE_CURRENT_SOURCE_DIR}/lib/dropbox_uploader/dropbox_uploader.sh ${DEPLOY_DIR}
+cp ${CMAKE_CURRENT_BINARY_DIR}/frontend/antifurto.fcgi ${DEPLOY_DIR}
+#cp ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/yowsup-cli ${DEPLOY_DIR}
+#cp -r ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/Yowsup ${DEPLOY_DIR}/Yowsup
+#cp -r ${CMAKE_CURRENT_SOURCE_DIR}/lib/yowsup/src/Examples ${DEPLOY_DIR}/Examples
+#cp ${CMAKE_CURRENT_SOURCE_DIR}/lib/dropbox_uploader/dropbox_uploader.sh ${DEPLOY_DIR}
 #cp test/antifurto_pi_test ${DEPLOY_DIR}
