29de48abe504f34d61c2db24a757119a24407950
Michele Bertasi <michele.bertasi@gmail.com>
2014-10-01 22:43:50 +0200

    [config] Removed unused

--- config/certs/new_ca.sh
+++ config/certs/new_ca.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-mkdir -p ca/private ca/certs
+rm -rf ca
+mkdir ca ca/private ca/certs ca/crl ca/newcerts
+touch ca/index.txt
+touch ca/serial
 openssl req -config ca/openssl.cnf -new -x509 -extensions v3_ca -keyout ca/private/ca.key -out ca/certs/ca.crt -days 7300
 chmod 0400 ca/private/ca.key
